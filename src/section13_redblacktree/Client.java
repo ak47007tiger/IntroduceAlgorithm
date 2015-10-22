2dbf51403ff606011288a2e7d51d2f138da2d119
mars <mars@doocom.cn>
2015-10-22 17:25:59 +0800

    add delete and delete fix up

--- src/section13_redblacktree/Client.java
+++ src/section13_redblacktree/Client.java
@@ -3,9 +3,13 @@ package section13_redblacktree;
 public class Client {
 
 	public static void main(String[] args) {
-		testInsert();
+		int[] keys = {6,3,8,4,5,9,2,1,10};
+		testInsert(keys);
+		int[] keys1 = {5,10,8,9,6,4,2,1,3};
+		testInsert(keys1);
+		testDelete();
 	}
-	public static void testInsert(){
+	public static void testDelete(){
 		int[] keys = {6,3,8,4,5,9,2,1,10};
 		RBnode nil = new RBnode();
 		nil.color = RBnode.Color.black;
@@ -15,7 +19,26 @@ public class Client {
 			z.key = keys[i];
 			rBtree.insert(z);			
 		}
-		rBtree.firstShow(rBtree.root());
+//		rBtree.firstShow(rBtree.root());
+		RBnode node = rBtree.delete(rBtree.search(8));
+		rBtree.firstShowDerict(rBtree.root);
+		System.out.println();
+		rBtree.insert(node);
+		rBtree.firstShowDerict(rBtree.root);
+		System.out.println();
+	}
+	public static void testInsert(int[] keys){
+		RBnode nil = new RBnode();
+		nil.color = RBnode.Color.black;
+		RBtree rBtree = new RBtree(nil);
+		for(int i = 0; i < keys.length; i++){
+			RBnode z = new RBnode();
+			z.key = keys[i];
+			rBtree.insert(z);			
+		}
+//		rBtree.firstShow(rBtree.root());
+		rBtree.firstShowDerict(rBtree.root);
+		System.out.println();
 	}
 }
 //6black(4red(2black(1red)(3red))(5black))(9black(8red)(10red))
--- src/section13_redblacktree/RBtree.java
+++ src/section13_redblacktree/RBtree.java
@@ -78,8 +78,6 @@ public class RBtree {
 		root.color = RBnode.Color.black;
 	}
 
-	
-
 	void leftRotate(RBnode x) {
 		RBnode y = x.right;
 		x.right = y.left;
@@ -120,21 +118,172 @@ public class RBtree {
 	}
 
 	public void firstShow(RBnode node) {
-		System.out
-				.print(String.format("%d[%s]", node.key, node.color.toString()));
+		System.out.print(String.format("%d[%s]", node.key,
+				node.color.toString()));
 		if (node.left != nil) {
-			System.out.print(String.format(" %dleft(",node.key));
+			System.out.print(String.format(" %dleft(", node.key));
 			firstShow(node.left);
 			System.out.print(")");
 		}
 		if (node.right != nil) {
-			System.out.print(String.format(" %dright(",node.key));
+			System.out.print(String.format(" %dright(", node.key));
 			firstShow(node.right);
+			System.out.print(")");
+		}
+	}
+	public void firstShowDerict(RBnode node) {
+		System.out.print(String.format("%d%s", node.key,
+				node.color.toString()));
+		if (node.left != nil) {
+			System.out.print(String.format("(", node.key));
+			firstShowDerict(node.left);
+			System.out.print(")");
+		}
+		if (node.right != nil) {
+			System.out.print(String.format("(", node.key));
+			firstShowDerict(node.right);
 			System.out.print(")");
 		}
 	}
 
 	public RBnode root() {
 		return root;
+	}
+
+	public RBnode delete(RBnode z) {
+		RBnode y;
+		if (z.left == nil || z.right == nil) {
+			y = z;
+		} else {
+			y = successor(z);
+		}
+		RBnode x;
+		if (y.left != nil) {
+			x = y.left;
+		} else {
+			x = y.right;
+		}
+		x.p = y.p;
+		if (y.p == nil) {
+			root = x;
+		} else {
+			if (y == y.p.left) {
+				y.p.left = x;
+			} else {
+				y.p.right = x;
+			}
+		}
+		if (y != z) {
+			z.key = y.key;
+			// copy y's satellite data into z
+		}
+		if (y.color == RBnode.Color.black) {
+			fixup_delete(x);
+		}
+		return y;
+	}
+
+	public RBnode successor(RBnode z) {
+		if (z.right != nil) {
+			return min(z.right);
+		}
+		while (z.p != nil && z.p.right == z) {
+			z = z.p;
+		}
+		return z.p;
+	}
+
+	public RBnode min(RBnode x) {
+		while (x.left != nil) {
+			x = x.left;
+		}
+		return x;
+	}
+
+	public void fixup_delete(RBnode x) {
+		while (x != root && x.color == RBnode.Color.black) {
+			RBnode w;
+			if (x == x.p.left) {
+				w = x.p.right;
+				if (w.color == RBnode.Color.red) {
+					//case1
+					w.color = RBnode.Color.black;
+					x.p.color = RBnode.Color.red;
+					leftRotate(x.p);
+					w = x.p.right;
+					//case1 end
+				}
+				if (w.left.color == RBnode.Color.black
+						&& w.right.color == RBnode.Color.black) {
+					//case2
+					w.color = RBnode.Color.red;
+					x = x.p;
+					//case2 end
+				} else {
+					if (w.right.color == RBnode.Color.black) {
+						//case3
+						w.left.color = RBnode.Color.black;
+						w.color = RBnode.Color.red;
+						rightRotate(w);
+						w = x.p.right;
+						//cae3 end
+					}
+					//case4
+					w.color = x.p.color;
+					x.p.color = RBnode.Color.black;
+					w.right.color = RBnode.Color.black;
+					leftRotate(x.p);
+					x = root;
+					//case4 end
+				}
+			}else{
+				w = x.p.left;
+				if (w.color == RBnode.Color.red) {
+					//case1
+					w.color = RBnode.Color.black;
+					x.p.color = RBnode.Color.red;
+					rightRotate(x.p);
+					w = x.p.left;
+					//case1 end
+				}
+				if (w.right.color == RBnode.Color.black
+						&& w.left.color == RBnode.Color.black) {
+					//case2
+					w.color = RBnode.Color.red;
+					x = x.p;
+					//case2 end
+				} else {
+					if (w.left.color == RBnode.Color.black) {
+						//case3
+						w.right.color = RBnode.Color.black;
+						w.color = RBnode.Color.red;
+						leftRotate(w);
+						w = x.p.left;
+						//case3 end
+					}
+					//case4
+					w.color = x.p.color;
+					x.p.color = RBnode.Color.black;
+					w.left.color = RBnode.Color.black;
+					rightRotate(x.p);
+					x = root;
+					//case4 end
+				}
+			}
+		}
+		x.color = RBnode.Color.black;
+	}
+	public RBnode search(int key){
+		RBnode curNode = root;
+		while(curNode != nil){
+			if(key < curNode.key){
+				curNode = curNode.left;
+			}else if(key > curNode.key){
+				curNode = curNode.right;
+			}else{
+				return curNode;
+			}
+		}
+		return curNode;
 	}
 }
